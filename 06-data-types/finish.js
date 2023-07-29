0f3be870468078ad90d106c972df3081c03f69c7
Nikita Sergeev <notscreamplay73@gmail.com>
2023-07-29 14:39:30 +0400

    Sixth Task

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,18 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const string = 'String',
+    number = 100,
+    boolean = true,
+    nullable = null,
+    undefinedVar = undefined,
+    object = {},
+    array = [];
+console.log(typeof(string));
+console.log(typeof(number));
+console.log(typeof(boolean));
+console.log(typeof(nullable));
+console.log(typeof(undefinedVar));
+console.log(typeof(object));
+console.log(typeof(array));
